37d9fe5effa80bbd2ead7ca17afde6df3ca8cb90
C V CHARAN <cherryiiit1234@gmail.com>
2025-08-10 19:31:19 +0530

    added sql files

---
+++ sql/1045.sql
@@ -0,0 +1,6 @@
+select distinct customer_id from Customer
+where customer_id in (
+    select customer_id from Customer
+    group by customer_id
+    having count(distinct product_key) = (select count(product_key) from Product)
+)

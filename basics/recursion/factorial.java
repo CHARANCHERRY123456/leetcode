5e01d480a8b26bac17ff43bf909d5bacf2552dc9
C V CHARAN <151058981+CHARANCHERRY123456@users.noreply.github.com>
2025-09-01 09:12:48 +0530

    Add recursive factorial function in factorial.java

--- basics/recursion/factorial.java
+++ basics/recursion/factorial.java
@@ -1,1 +1,18 @@
+// written by my own not compiled so may get errorrs
+import java.util.Scanner;
+class Main{
+    // here we can use long if we consider the n>20
+    static int fact(int n){
+        if(n <= 1) return 1;
+        return n * fact(n-1);
+    }
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int factorial = fact(n);
+        System.out.println(factorial);
+    }
+}
 
+// time complextiy : O(n)
+// space complexity : O(n)
